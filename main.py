954c56ba3776fa38d7da0d2d94293cd882208b78
woojune511 <geonju511@naver.com>
2021-09-14 02:00:15 +0900

    template applied

--- main.py
+++ main.py
@@ -1,75 +1,100 @@
-'''
-Given the number of nodes N (typically, power of 2), the number of edges E, and four prob. params p_a, p_b, p_c, and p_d,
-generate a NxN directed graph.
-There can be duplicate edges; keep only one of them.
-Add edges until the graph has E edges.
-
-usage: main.py [-h] [-n NUMNODES] [-e NUMEDGES] [-a PA] [-b PB] [-c PC] [-o OUTPUTNAME]
-
-optional arguments:
--n NUMNODES, --numNodes NUMNODES
-Select the number of nodes of the graph.
--e NUMEDGES, --numEdges NUMEDGES
-Select the number of edges of the graph.
--a PA, --pA PA
-The probability of the edge assignment in the partition a.
--b PB, --pB PB
-The probability of the edge assignment in the partition b.
--c PC, --pC PC
-The probability of the edge assignment in the partition c.
--o OUTPUTNAME, --output OUTPUTNAME
-The file name of an output matrix.
-'''
-
-import numpy as np
-import scipy
 import argparse
+import numpy as np
+from scipy import sparse
 
-class hw1(object):
-    def __init__(self):
-        return
+def genRMat(N, E, probs, output_path):
+    print('numNodes:', N)
+    print('numEdges:', E)
+    print('probabilites: (pA, pB, pC, pD) =', '(' + ','.join(map(str, probs)) + ')')
+    print('output_path:', output_path)
+    matrix = np.zeros((N, N), dtype=np.int64)
+    
+    ### TODO: WRITE YOUR CODE HERE. YOU SHOULD *NOT* MODIFY THE OTHER PARTS! ###    
+    matrix = sparse.csr_matrix(matrix)
+    ############################################################################
+    
+    # Validation for outputs. DO *NOT* MODIFY THIS PART!
+    valid = True
+    if matrix.dtype != np.int64:
+        print(f'ERROR: Wrong datatype. (Expected: np.int64, but found {matrix.dtype}.)')
+        valid = False
+    if matrix.shape != (N, N):
+        print(f'ERROR: Wrong shape. (Expected: ({N}, {N}), but found {matrix.shape}.)')
+        valid = False
+    if matrix.min() != 0 or matrix.max() != 1:
+        print(f'ERROR: The output should be nonnegative, and should be binary.')
+        valid = False
+    if matrix.sum() != E:
+        print(f'ERROR: The number of non-zero entries in the output shoule be {E}, but found {matrix.sum()}')
+        valid = False
+        
+    if valid:
+        print(f'Pretest passed! However, it does not guarantee the correctness of your code.')
+        try:
+            sparse.save_npz(output_path, matrix)
+        except:
+            print(f'Failed to save the output. Please check the output path again.')
+    else:
+        print('ERROR: Validation for the output matrix failed. Please check your code again.')
+    
+        
+if __name__ == '__main__':
+    """
+    Generating a graph by R-MAT.
+    optional arguments:
+        -n NUMNODES, --numNodes NUMNODES Select the number of nodes of the graph.
+        -e NUMEDGES, --numEdges NUMEDGES Select the number of edges of the graph.
+        -a PA, --pA PA The probability of the edge assignment in the partition a.
+        -b PB, --pB PB The probability of the edge assignment in the partition b.
+        -c PC, --pC PC The probability of the edge assignment in the partition c.
+        -o OUTPUTNAME, --output OUTPUTNAME The file name of an output matrix.
 
-
-def main():
-    n, e, a, b, c, o = [None]*6
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--numNodes", help="Select the number of nodes of the graph.")
-    parser.add_argument("-e", "--numEdges", help="Select the number of edges of the graph.")
-    parser.add_argument("-a", "--pA", help="The probability of the edge assignment in the partition a.")
-    parser.add_argument("-b", "--pB", help="The probability of the edge assignment in the partition b.")
-    parser.add_argument("-c", "--pC", help="The probability of the edge assignment in the partition c.")
-    parser.add_argument("-o", "--output", help="The file name of an output matrix.")
+    Example:
+        python main.py -n 128 -e 1000 -a 0.5 -b 0.2 -c 0.1 -o example.npy
+    """
+    parser = argparse.ArgumentParser(description='Generating a graph by R-MAT.')
+    parser.add_argument("-n", "--numNodes", metavar="NUMNODES", type=int, default=128,
+                            help="Select the number of nodes of the graph.")
+    parser.add_argument("-e", "--numEdges", metavar="NUMEDGES", type=int, default=1000,
+                            help="Select the number of edges of the graph.")
+    parser.add_argument("-a", "--pA", metavar="PA", type=float, default=0.25,
+                            help="The probability of the edge assignment in the partition a.")
+    parser.add_argument("-b", "--pB", metavar="PB", type=float, default=0.25,
+                            help="The probability of the edge assignment in the partition b.")
+    parser.add_argument("-c", "--pC", metavar="PC", type=float, default=0.25,
+                            help="The probability of the edge assignment in the partition c.")
+    parser.add_argument("-o", "--output", metavar="OUTPUT", type=str, default="example.npy",
+                            help="The file name of an output matrix.")
     args = parser.parse_args()
     
-    if args.numNodes:
-        n = args.numNodes
-    if args.numEdges:
-        e = args.numEdges
-    if args.pA:
-        a = args.pA
-    if args.pB:
-        b = args.pB
-    if args.pC:
-        c = args.pC
-    if args.output:
-        o = args.output
-    else:
-        o = "output"
-
-    return genRMat(n,e,a,b,c,o)
-
-
-'''
-Recursively adds edges in given adjacency matrix.
-'''
-def genRMat(n,e,a,b,c,o):
-    return
-
-
-if __name__ == "__main__":
-    main()
-
-
-# Where should I declare the adjacency matrix?
-# If it is defined in genRMat(), the recursive function will require too much memory.
-# It is not desirable.
+    # Validation for inputs. DO *NOT* MODIFY THIS PART!
+    valid = True
+    if type(args.numNodes) != int or args.numNodes <= 0:
+        print('ERROR: numNodes should be non-negative.')
+        valid = False
+    if type(args.numEdges) != int or args.numEdges <= 0 or args.numEdges > (args.numNodes * args.numNodes):
+        print('ERROR: numEdges should be non-negative, and should not be exceed numNodes^2.')
+        valid = False
+    if type(args.pA) != float or args.pA < 0 or args.pA > 1:
+        print('ERROR: Probabilites should be non-negative, and should not exceed 1.')
+        valid = False
+    if type(args.pB) != float or args.pB < 0 or args.pB > 1:
+        print('ERROR: Probabilites should be non-negative, and should not exceed 1.')
+        valid = False
+    if type(args.pC) != float or args.pC < 0 or args.pC > 1:
+        print('ERROR: Probabilites should be non-negative, and should not exceed 1.')
+        valid = False
+    if type(args.pA) != float or type(args.pB) != float or type(args.pC) != float or not (0 <= (args.pA + args.pB + args.pC) <= 1):
+        print('ERROR: Probabilites should be non-negative, and should not exceed 1.')
+        valid = False
+    if type(args.output) != str or len(args.output) == 0:
+        print('ERROR: You should specify the path of an output matrix.')
+        valid = False
+    if not valid:
+        print('ERROR: Validation for inputs failed. Please check your code again.')
+        exit(0)
+        
+    N, E = args.numNodes, args.numEdges
+    probs = [args.pA, args.pB, args.pC, 1 - (args.pA + args.pB + args.pC)]
+    output_path = args.output
+    genRMat(N, E, probs, output_path)
